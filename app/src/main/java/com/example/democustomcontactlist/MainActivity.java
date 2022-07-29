d9c2a4faee6252ce4dec845c189788aec9e44ac8
yeowhengg <20042741@myrp.edu.sg>
2022-07-29 11:57:01 +0800

    section a, b

--- app/src/main/java/com/example/democustomcontactlist/MainActivity.java
+++ app/src/main/java/com/example/democustomcontactlist/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.democustomcontactlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lvContact;
+    ArrayList<Contact> alContactList;
+    CustomAdapter caContact;
+    ImageView ivPics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Picasso.with(this).setLoggingEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvContact = findViewById(R.id.listViewContacts);
+        alContactList = new ArrayList<>();
+        Contact item1 = new Contact("Mary", 65, 1234567, 'F');
+        Contact item2 = new Contact("Ken", 65, 7654321, 'M');
+        alContactList.add(item1);
+        alContactList.add(item2);
+
+        caContact = new CustomAdapter(this, R.layout.row, alContactList);
+        lvContact.setAdapter(caContact);
+
+        ivPics = findViewById(R.id.imageId);
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(ivPics);
+
     }
 }
